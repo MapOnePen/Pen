e8fdcf87bb6cecb7c5b9ce355d083ff4aee2824a
jiangyuanlong <jiangyl@sinoauto.com>
2017-10-16 15:47:36 +0800

    添加 表达试扩展

---
+++ 后台/Infrastructure/Extensions/ExpressionExtension/ExpressionExtension/ExpressionFun.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ExpressionExtension
+{
+    /// <summary>
+    /// 扩展 Expression
+    /// </summary>
+    public static class ExpressionFun
+    {
+        /// <summary>
+        /// 扩展 and的运算符
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="expr1"></param>
+        /// <param name="expr2"></param>
+        /// <returns></returns>
+        public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2)
+        {
+            var invokedExpr = Expression.Invoke(expr2, expr1.Parameters);
+
+            return Expression.Lambda<Func<T, bool>>(Expression.AndAlso(expr1.Body, invokedExpr), expr1.Parameters);
+        }
+        /// <summary>
+        /// 扩展or的运算符
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="expr1"></param>
+        /// <param name="expr2"></param>
+        /// <returns></returns>
+        public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2)
+        {
+            var invokedExpr = Expression.Invoke(expr2, expr1.Parameters);
+
+            return Expression.Lambda<Func<T, bool>>(Expression.OrElse(expr1.Body, invokedExpr), expr1.Parameters);
+        }
+    }
+}
